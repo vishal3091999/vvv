aa7f9c9f1b8a98ef29bab7e6760a470faea3ccb3
vishal3091999 <vishalmv1999@gmail.com>
2017-06-30 10:57:52 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the details:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the %d number is even:");
+}
+else
+printf("the %d number is odd:");
+getch();
+}
 
